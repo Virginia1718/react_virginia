8607ae473dec5aaf5c6d500700dffcafca3c3583
Virginia1718 <12410231@student.unklab.ac.id>
2025-11-02 17:28:49 +0800

    initial commit for React project setup

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// komponen adalah fungsi yang mengreturn suatu JSX
+//JSX baris 5-7 dan itu bukan html dan itu adalah fungsi
+//baris 4-5 adalah javascript
+    //jsx hanya punya 1 div atau 1 kelas
+    //div sbgai penampung komponen lain
+    //kurung kurawal artinya memasukkan js kedalm jsx
+    //<> ini adalah fragment dan sudah dianggp parent clss, ini sama dengan div namun tidak bisa di edit
+//Komponen adalah fungsi yang return JSX
+import HelloWorld from "./HelloWorld";
+
+const App = () => {
+  let hello = "Hello World";
+  return (
+    <>
+      <div className="text-[25px] bg-blue-500 m-10 p-10 font-serif">
+        {hello}
+      </div>
+      <HelloWorld />
+    </>
+  );
+};
+
+export default App;
